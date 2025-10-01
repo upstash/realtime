e1cfcd953c4fc1ff24913cacdba972088922120e
Josh <josh@upstash.com>
2025-10-01 20:58:44 +0200

    refactor: clean up type definitions

--- src/types.ts
+++ src/types.ts
@@ -13,12 +13,3 @@ export type UserEvent<T = unknown> = {
 export type RealtimeMessage<T = unknown> = SystemEvent | UserEvent<T>
 
 export type ConnectionStatus = "connected" | "disconnected" | "error" | "connecting"
-
-export const isSystemEvent = (msg: RealtimeMessage): msg is SystemEvent => {
-  return "type" in msg
-}
-
-export const isUserEvent = <T = unknown>(msg: RealtimeMessage<T>): msg is UserEvent<T> => {
-  return "__event_path" in msg && "__stream_id" in msg
-}
-
